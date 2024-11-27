c0e5725b78e5054a54152af9399d6c0afa4740c7
SJReddy1 <122619223+SJReddy1@users.noreply.github.com>
2024-11-27 20:23:36 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,20 +1,14 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectTimeout: 10000,
-});
+// Use the full connection string from DB_URL
+const connection = mysql.createConnection(process.env.DB_URL);
 
 connection.connect(err => {
     if (err) throw err;
     console.log('Database connected!');
 });
-
 
 module.exports = connection;
